af4e8d2734499351eb533d3db058e6a0990367ab
Marina Popova <ppine7@gmail.com>
2021-02-24 22:24:10 -0500

    minor updates from the Lab

---
+++ fp-java-homework/src/main/java/cscie88a/week3_fp/ITrainable4.java
@@ -0,0 +1,10 @@
+package cscie88a.week3_fp;
+
+/**
+ * FI with multiple arguments
+ */
+@FunctionalInterface
+public interface ITrainable4 {
+
+    ActionResult myNewMethod(String trick1, String trick2);
+}
