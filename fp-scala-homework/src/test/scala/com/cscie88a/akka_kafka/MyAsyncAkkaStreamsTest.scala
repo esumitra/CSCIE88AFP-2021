46676716da314833ee9b5f5c1ef0baefbeaca9b2
esumitra <edward.sumitra@gmail.com>
2021-04-12 19:33:41 -0400

    Akka intermediate (#16)
    * add akka async scaffolding
    
    * add alpakka-kafka scaffolding
    
    Co-authored-by: esumitra <esumitra@cainc.com>

---
+++ fp-scala-homework/src/test/scala/com/cscie88a/akka_kafka/MyAsyncAkkaStreamsTest.scala
@@ -0,0 +1,18 @@
+package com.cscie88a.akka_kafka
+
+import akka.stream.scaladsl._
+import com.cscie88a.AkkaSpec
+
+import scala.concurrent.Future
+
+class MyAsyncAkkaStreamsTest extends AkkaSpec {
+
+  "empty stream should have size 0" in {
+    Source.empty.runWith(Sink.seq).map { s =>
+      s.size should be (0)
+    }
+  }
+
+  // add tests below
+
+}
